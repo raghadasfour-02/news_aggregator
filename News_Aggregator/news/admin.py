f9ae5627c155e7019180ccd61f6525f69d82b469
raghadasfour-02 <r2asfour@uwaterloo.ca>
2023-01-01 17:10:28 -0500

    Create Feed Model

--- News_Aggregator/news/admin.py
+++ News_Aggregator/news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Feed, Article
+from .models import Feed
 # Register your models here.
 
 admin.site.register(Feed)
-admin.site.register(Article)
+# admin.site.register(Article)
--- News_Aggregator/news/models.py
+++ News_Aggregator/news/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-
-class Article(models.Model):
-    feed = models.ForeignKey(on_delete=models.deletion.CASCADE, to='news.Feed')
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
-
 class Feed(models.Model):
     title = models.CharField(max_length=150)
 
