971f9fb6b6afed7a7b7549157bfa7852f36f670c
raghadasfour-02 <r2asfour@uwaterloo.ca>
2023-01-01 16:21:03 -0500

    Fix WSGI Application Configuration

--- News_Aggregator/News_Aggregator/settings.py
+++ News_Aggregator/News_Aggregator/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'newsproject.urls'
+ROOT_URLCONF = 'News_Aggregator.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'newsproject.wsgi.application'
+WSGI_APPLICATION = 'News_Aggregator.wsgi.application'
 
 
 # Database
